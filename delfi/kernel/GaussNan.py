d7c33851f72e371f2ba7104c7da64b3312e916b6
Cornelius <cornelius.schroeder@gmail.com>
2019-01-24 17:14:33 +0100

    added GaussNan kernel

---
+++ delfi/kernel/GaussNan.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+from delfi.kernel.BaseKernel import BaseKernel
+
+"""
+simple hack to avoid Nans in the network if imputed=True
+TODO: better solution would be (?) to calculate kernel on imputed values
+"""
+
+class GaussNan(BaseKernel):
+    @staticmethod
+    def kernel(u):
+        if np.isnan(u):
+            # not that clear what value to choose, but should not be that important
+            return 0.5
+        else:
+            return 1 / np.sqrt(2 * np.pi) * np.exp(-0.5 * u ** 2)
+
