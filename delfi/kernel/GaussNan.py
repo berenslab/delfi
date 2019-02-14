3af780df62eb9606b9a7a90b12b2d4fafa5ae1b4
Cornelius <cornelius.schroeder@gmail.com>
2019-02-14 13:38:45 +0100

    Added HalfGaussNan kernel

--- delfi/kernel/GaussNan.py
+++ delfi/kernel/GaussNan.py
@@ -16,3 +16,13 @@ class GaussNan(BaseKernel):
         else:
             return 1 / np.sqrt(2 * np.pi) * np.exp(-0.5 * u ** 2)
 
+class HalfGaussNan(BaseKernel):
+    @staticmethod
+    def kernel(u):
+        if np.isnan(u):
+            # not that clear what value to choose, but should not be that important
+            return 0.5
+        if u <= 0.0 :
+            return 1.0
+        else:
+            return 1/np.sqrt(2*np.pi)*np.exp(-0.5*u**2)
