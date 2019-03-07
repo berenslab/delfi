f94c61f0f51e41dd216bd9ddc063e5c91cfd69db
jonathanoesterle <jonathan.oesterle1@gmail.com>
2019-03-07 15:04:34 +0000

    Fix HalfGauss

--- delfi/kernel/Gauss.py
+++ delfi/kernel/Gauss.py
@@ -15,6 +15,6 @@ class HalfGauss(BaseKernel):
     @staticmethod
     def kernel(u):
         if u <= 0.0 :
-            return 1.0
+            return 1/np.sqrt(2*np.pi)
         else:
             return 1/np.sqrt(2*np.pi)*np.exp(-0.5*u**2)
